6cf0f0cc8df4a15f6d3db208acb786f5944ff050
Marleen ( 玛琳) <handyyou@live.nl>
2023-11-09 02:26:30 +0100

    Start project

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let menu = document.querySelector('#menu-bars');
+    let navbar = document.querySelector('.navbar');
+
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+        menu.classList.toggle('fa-times');
+    }
+    
+
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+        menu.classList.remove('fa-times');
+    }
+});
